c3ba683ed68f7e240e3bb25667ed22534de07ccb
georgibaltiev <gbaltiev@gmail.com>
2026-06-04 13:58:36 +0300

    add: filter for cadastre buildings

--- data_parser/cadastre_data_parser.py
+++ data_parser/cadastre_data_parser.py
@@ -10,10 +10,10 @@ geojson_prefix = "geojson"
 geojson_suffix = ".geojson"
 buildings_filename = "buildings"
 
-def merge_geojsons(geojson_paths, output_path):
+def merge_geojsons(geojson_paths, args):
     
     if len(geojson_paths) == 1:
-        os.rename(geojson_paths[0], output_path)
+        os.rename(geojson_paths[0], args.output_geojson)
         return
 
     gpds = []
@@ -23,16 +23,20 @@ def merge_geojsons(geojson_paths, output_path):
     print(f"Merging GeoJSONs")
     merged = gpd.GeoDataFrame(pd.concat(gpds, ignore_index=True), crs=gpds[0].crs)
 
-    print(f"Saving GeoJSON to: {output_path}")
-    merged.to_file(output_path, driver="GeoJSON")
+    print(f"Saving GeoJSON to: {args.output_geojson}")
+    merged.to_file(args.output_geojson, driver="GeoJSON")
 
 def parse_shapefiles(args):
 
     geojson_paths = []
     
     for shp_path in args.shp_paths.split(","):
-        
+
         gdf = gpd.read_file(shp_path)
+        
+        if args.filter:
+            gdf = gdf[gdf['functype'].str.contains(args.filter, case=False, regex=True, na=False)]
+        
         shp_name = os.path.basename(shp_path).split(".")[0]
         
         gdf = gdf.to_crs(epsg=4326)
@@ -53,14 +57,14 @@ def parse_shapefiles(args):
 def main():
 
     parser = argparse.ArgumentParser(description='Parse parameters for the shp converter')
-    parser.add_argument('--shp_paths', help='Path to shapefiles. Should be comma-separated files if you want to specify multiple of them.')
-    # parser.add_argument('--limit', type=int, default=None, help='Limit number of features (useful for testing)')
-    parser.add_argument('--filter', dest='filter_type', default=None, help='Filter which buildings should be examined"')
-    
+    parser.add_argument('--shp_paths', dest='shp_paths', help='Path to shapefiles. Should be comma-separated files if you want to specify multiple of them.')
+    parser.add_argument('--filter', dest='filter', help='Filter which buildings should be included in the GeoJSON.')
+    parser.add_argument('--output_geojson', dest='output_geojson', help='Specify where to save the GeoJSON file.', default=f"{data_prefix}/{geojson_prefix}/{buildings_filename}{geojson_suffix}")
+
     args = parser.parse_args()
 
     geojsons = parse_shapefiles(args)
-    merge_geojsons(geojsons, f"{data_prefix}/{geojson_prefix}/{buildings_filename}{geojson_suffix}")
+    merge_geojsons(geojsons, args)
 
 if __name__ == '__main__':
     main()
